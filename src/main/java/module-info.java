558050a683771585c3507f99c6eebcbde3b8335e
Taras Zhyravetskyi <tarik.zhyravetskyy@gmail.com>
2023-06-26 09:36:53 +0300

    toGit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.puzzlefx {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires net.synedra.validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires com.almasb.fxgl.all;
+    requires lombok;
+
+    opens com.example.puzzlefx to javafx.fxml;
+    exports com.example.puzzlefx;
+}
